58b78f40ef02468dbbdf297324cdc8f9e833cd06
aoi <goto-aoi@users.noreply.github.com>
2024-02-25 17:52:28 +0900

    ライブラリのタブを作成

--- lib/pages/library_page.dart
+++ lib/pages/library_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+///TODO あまり納得いってないよこのタブ
+///あと、riverpod使わずにstateful使っちゃってるのちょっと罪悪感あるし！
 
 class LibraryPage extends StatefulWidget {
   const LibraryPage({super.key});
@@ -7,32 +11,70 @@ class LibraryPage extends StatefulWidget {
   State<LibraryPage> createState() => _LibraryPageState();
 }
 
-class _LibraryPageState extends State<LibraryPage> {
+class _LibraryPageState extends State<LibraryPage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _tabController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      initialIndex: 1,
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          flexibleSpace: const TabBar(
-            tabs: <Widget>[
-              Tab(icon: Icon(Icons.favorite),text: 'お気に入り'),
-              Tab(icon: Icon(Icons.history),text: '閲覧履歴'),
-            ],
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        flexibleSpace: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            decoration: BoxDecoration(
+                color: Colors.grey.shade300, borderRadius: BorderRadius.circular(25.0)),
+            child: Expanded(
+              child: TabBar(
+                controller: _tabController,
+                indicator: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25.0),
+                  color: Colors.red,
+                ),
+                labelColor: Colors.white,
+                unselectedLabelColor: Colors.black,
+                tabs: [
+                  _Tab(label: 'お気に入り', icon: const Icon(Icons.favorite)),
+                  _Tab(label: '閲覧履歴', icon: const Icon(Icons.history)),
+                ],
+              ),
+            ),
           ),
-          automaticallyImplyLeading: false, //戻るボタンの消去
         ),
-        body: const TabBarView(
-          children: <Widget>[
-            Center(
-              child: Text("お気に入りのリスト表示",style: TextStyle(fontSize: 20)),
-            ),
-            Center(
-              child: Text("閲覧履歴のリスト表示",style: TextStyle(fontSize: 20)),
-            ),
-          ],
-        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: const <Widget>[
+          Text('aaa'),
+          Text('bbb')
+        ], //TODO,, tabcontrollerがindexを管理してくれているから、順番に作っちゃうだけでおけ
+      ),
+    );
+  }
+
+  Widget _Tab({required String label, required Icon icon}) {
+    return Tab(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          icon,
+          const SizedBox(width: 8.0),
+          Text(label),
+        ],
       ),
     );
   }
