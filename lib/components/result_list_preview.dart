4be0f12dee9e82b5559c96b7154d49d783123d99
aoi <goto-aoi@users.noreply.github.com>
2024-03-11 11:40:51 +0900

    フィルター機能完成

--- lib/components/result_list_preview.dart
+++ lib/components/result_list_preview.dart
@@ -1,68 +1,81 @@
+import 'package:algolia/algolia.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:kenryo_tankyu/components/favorite.dart';
 
 class ResultList extends StatelessWidget {
-  const ResultList({super.key});
+  final List<AlgoliaObjectSnapshot> data;
+  const ResultList({super.key, required this.data});
 
   @override
   Widget build(BuildContext context) {
     return Flexible(
       child: ListView.separated(
         itemBuilder: (BuildContext context, int index) {
+          final content = data[index];
           return Padding(
             padding: const EdgeInsets.all(8.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Container(
-                          width: 16,
-                          height: 16,
-                          decoration: const BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.blue,
-                          ),
-                        ),
-                        const SizedBox(width: 8),
-                        const Text('2023年度'),
-                        const SizedBox(width: 8),
-                        const Text('探究科'),
-                      ],
-                    ),
-                    const SizedBox(height: 4),
-                    const Text('タイトル', style: TextStyle(fontSize: 20)),
-                    const Row(
-                      children: [
-                        Chip(
-                            label: Text(
-                              '環境 > 生物保護',
-                              style: TextStyle(fontSize: 14),
+                Flexible(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            width: 16,
+                            height: 16,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.blue,
                             ),
-                            visualDensity: VisualDensity(vertical: -4),
-                            padding: EdgeInsets.zero),
-                        SizedBox(width: 8),
-                        Chip(
-                            label: Text('地域 > 京都'),
-                            visualDensity: VisualDensity(vertical: -4),
-                            padding: EdgeInsets.zero),
-                      ],
-                    ),
-                  ],
+                          ),
+                          const SizedBox(width: 8),
+                          Text('${content.data['year']}年度入学'),
+                          const SizedBox(width: 8),
+                          Text(content.data['course']),
+                        ],
+                      ),
+                      const SizedBox(height: 4),
+                      Text(content.data['title'], style: const TextStyle(fontSize: 20),softWrap: true),
+                      const SizedBox(height: 4),
+                      SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children: [
+                            Chip(
+                                label: Text(
+                                  '${content.data['category1']} > ${content.data['subCategory1']}',
+                                  style: const TextStyle(fontSize: 14),
+                                ),
+                                visualDensity: const VisualDensity(vertical: -4),
+                                padding: EdgeInsets.zero),
+                            const SizedBox(width: 8),
+                            Chip(
+                                label: Text('${content.data['category2']} > ${content.data['subCategory2']}',style: const TextStyle(fontSize: 14)),
+                                visualDensity: const VisualDensity(vertical: -4),
+                                padding: EdgeInsets.zero),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-                const Column(
-                  children: [
-                    Image(
-                      image: AssetImage('images/category/art.png'),
-                      width: 50,
-                      height: 50,
-                    ),
-                    Favorite(),
-                  ],
+                const Padding(
+                  padding: EdgeInsets.only(left:8.0),
+                  child: Column(
+                    children: [
+                      Image(
+                        image: AssetImage('images/category/art.png'),
+                        width: 50,
+                        height: 50,
+                      ),
+                      Favorite(),
+                    ],
+                  ),
                 ),
               ],
             ),
@@ -77,7 +90,7 @@ class ResultList extends StatelessWidget {
             ),
           );
         },
-        itemCount: 5,
+        itemCount: data.length,
       ),
     );
   }
