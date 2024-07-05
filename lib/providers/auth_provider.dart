0a57f4533c5f7d7971a39b2f544dbbeace23c2ea
aoi <goto.aoi.516@gmail.com>
2024-07-05 20:34:51 +0900

    こけんときょーゆー

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -2,5 +2,10 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final authStateChangesProvider = StreamProvider<User?>((ref) {
-  return FirebaseAuth.instance.authStateChanges();
+  FirebaseAuth.instance.userChanges().listen((User? user) {
+    if (user = null) {
+      return;
+    }
+    return user;
+  });
 });
